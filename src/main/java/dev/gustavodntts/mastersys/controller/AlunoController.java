b468e1d6b36c9c941b0e535b5e0305aa21c795d1
Gustavo Dantas <gustavo.dantas.marim.dev@gmail.com>
2026-06-16 11:31:17 -0300

    feat(aluno): adiciona crud para alunos

---
+++ src/main/java/dev/gustavodntts/mastersys/controller/AlunoController.java
@@ -0,0 +1,48 @@
+package dev.gustavodntts.mastersys.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import dev.gustavodntts.mastersys.dto.AlunoRequest;
+import dev.gustavodntts.mastersys.dto.AlunoResponse;
+import dev.gustavodntts.mastersys.service.AlunoService;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AlunoResponse cadastrar(@RequestBody AlunoRequest alunoRequest) {
+        return alunoService.cadastrar(alunoRequest);
+    }
+
+    @GetMapping
+    public Page<AlunoResponse> listar(Pageable pageable) {
+        return alunoService.listar(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public AlunoResponse buscarPorId(@PathVariable Long id) {
+        return alunoService.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public AlunoResponse atualizar(@PathVariable Long id, @RequestBody AlunoRequest alunoRequest) {
+        return alunoService.atualizar(id, alunoRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Long id) {
+        alunoService.excluir(id);
+    }
+}
--- src/main/java/dev/gustavodntts/mastersys/repository/AlunoRepository.java
+++ src/main/java/dev/gustavodntts/mastersys/repository/AlunoRepository.java
@@ -4,5 +4,6 @@ import dev.gustavodntts.mastersys.domain.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    
+
+    boolean existByEmail(String email);
 }
